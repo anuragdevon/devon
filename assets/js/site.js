84180c0865824960367968a0e06986f79ef2ed9c
anuragdevon <msm19b021@iiitdm.ac.in>
2021-06-20 23:22:20 +0530

    lot of fixes

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,139 +1,109 @@
-let c = init("canvas"),
-  w = (canvas.width = window.innerWidth),
-  h = (canvas.height = window.innerHeight);
-//initiation
+// //<== Canvas ==>
+// function init(elementid){
+//   let canvas = document.getElementById(elementid),
+//       p = canvas.getContext("2d"),
+//       // w = (canvas.width = window.innerWidth),
+//       w = (canvas.width = window.innerWidth),
+//       h = (canvas.height = window.innerWidth);
+//       // h = (canvas.height = window.innerHeight);
+//       // console.log(w, h);
+//   p.fillStyle = "rgba(0, 0, 0, 1)";
+//   p.fillRect(0, 0, w, h);
+// //   console.log(p);
+//   initvalue = [p, w, h];
+//   return initvalue;
+// }
+// // initializing the canvas
+// let pr = init("dotta");
+// let p = pr[0];
+// let w = pr[1];
+// let h = pr[2];
+// // console.log(p, w, h);
+// class stars{
+//   constructor(){
+//       this.x = Math.random()*w;
+//         //   console.log("this is x",this.x);
+//       this.y = Math.random()*h;
+//         //   console.log("this is y",this.y);
+//       this.intensity= Math.random()*2;
+//         //   console.log("this is d",this.intensity);
+//       this.angle = Math.random()*2*Math.PI;
+//         //   console.log("this is a",this.angle);
+//       this.velocity = ((this.intensity)^2)/4;
+//         //   console.log("this is v",this.velocity);
 
-class firefly{
-  constructor(){
-    this.x = Math.random()*w;
-    this.y = Math.random()*h;
-    this.s = Math.random()*2;
-    this.ang = Math.random()*2*Math.PI;
-    this.v = this.s*this.s/4;
+//   }
+//   move(){
+//       this.x += this.velocity*Math.cos(this.angle);
+//       this.y += this.velocity*Math.sin(this.angle);
+//       this.angle += Math.random()*20*Math.PI/180-10*Math.PI/180;
+//     //   console.log(this.x, this.y, this.angle);
+//   }
+//   show(){
+//       p.beginPath();
+//     //   console.log(p.beginPath());
+//       p.arc(this.x, this.y, this.intensity, 0, 2*Math.PI);
+//     //   console.log(p.arc(this.x, this.y, this.intensity, 0, 2*Math.PI));
+//       p.fillStyle = "#149ddd";
+//       p.fill();
+//   }
+// } 
+
+// // array of stars
+// let s = []
+// function display() {
+//   if(s.length <50){
+//       s.push(new stars());
+//   }
+//   // move and show
+//   for(var i = 0; i < s.length; ++i){
+//       s[i].move();
+//       s[i].show();
+//       if(s[i].x < 0 || s[i].x > w ||  s[i].y < 0 || s[i].y > h ){
+//           s.splice(i, 1);
+//       }
+//   }
+// }
+// function animations(){
+//   window.requestAnimationFrame;
+//   p.clearRect(0,0,w,h);
+//   display();
+// }
+// animations();
+// setInterval(animations, 100);
+
+
+//<== contact form ==>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// contact button
+var mybutton = document.getElementById("mybtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+
+//<== Navbar shrink and Contact Button ==>
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0rem 0rem";
+    // document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "0.3rem 0rem";
+    // document.getElementById("logo").style.fontSize = "35px";
   }
-  move(){
-    this.x += this.v*Math.cos(this.ang);
-    this.y += this.v*Math.sin(this.ang);
-    this.ang += Math.random()*20*Math.PI/180-10*Math.PI/180;
-  }
-  show(){
-    c.beginPath();
-    c.arc(this.x,this.y,this.s,0,2*Math.PI);
-    c.fillStyle="#fddba3";
-    c.fill();
+
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    // mybutton.style.display = "block";
+    mybutton.style.bottom = "-4px"
+  } else {
+    // mybutton.style.display = "none";
+    mybutton.style.bottom = "-40px"
   }
 }
 
-let f = [];
-
-function draw() {
-  if(f.length < 100){
-    for(let j = 0; j < 10; j++){
-     f.push(new firefly());
-  }
-     }
-  //animation
-  for(let i = 0; i < f.length; i++){
-    f[i].move();
-    f[i].show();
-    if(f[i].x < 0 || f[i].x > w || f[i].y < 0 || f[i].y > h){
-       f.splice(i,1);
-       }
-  }
-}
-
-let mouse = {};
-let last_mouse = {};
-
-canvas.addEventListener(
-  "mousemove",
-  function(e) {
-    last_mouse.x = mouse.x;
-    last_mouse.y = mouse.y;
-
-    mouse.x = e.pageX - this.offsetLeft;
-    mouse.y = e.pageY - this.offsetTop;
-  },
-  false
-);
-function init(elemid) {
-  let canvas = document.getElementById(elemid),
-    c = canvas.getContext("2d"),
-    w = (canvas.width = window.innerWidth),
-    h = (canvas.height = window.innerHeight);
-  c.fillStyle = "rgba(30,30,30,1)";
-  c.fillRect(0, 0, w, h);
-  return c;
-}
-
-window.requestAnimFrame = (function() {
-  return (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function(callback) {
-      window.setTimeout(callback);
-    }
-  );
-});
-
-function loop() {
-  window.requestAnimFrame(loop);
-  c.clearRect(0, 0, w, h);
-  draw();
-}
-
-window.addEventListener("resize", function() {
-  (w = canvas.width = window.innerWidth),
-  (h = canvas.height = window.innerHeight);
-  loop();
-});
-console.log(w, h)
-loop();
-setInterval(loop, 1000 / 60);
-
-
-//------------------------------------------------------
-var alertRedInput = "#8C1010";
-var defaultInput = "rgba(10, 180, 180, 1)";
-
-function userNameValidation(usernameInput) {
-    var username = document.getElementById("username");
-    var issueArr = [];
-    if (/[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(usernameInput)) {
-        issueArr.push("No special characters!");
-    }
-    if (issueArr.length > 0) {
-        username.setCustomValidity(issueArr);
-        username.style.borderColor = alertRedInput;
-    } else {
-        username.setCustomValidity("");
-        username.style.borderColor = defaultInput;
-    }
-}
-
-function passwordValidation(passwordInput) {
-    var password = document.getElementById("password");
-    var issueArr = [];
-    if (!/^.{7,15}$/.test(passwordInput)) {
-        issueArr.push("Password must be between 7-15 characters.");
-    }
-    if (!/\d/.test(passwordInput)) {
-        issueArr.push("Must contain at least one number.");
-    }
-    if (!/[a-z]/.test(passwordInput)) {
-        issueArr.push("Must contain a lowercase letter.");
-    }
-    if (!/[A-Z]/.test(passwordInput)) {
-        issueArr.push("Must contain an uppercase letter.");
-    }
-    if (issueArr.length > 0) {
-        password.setCustomValidity(issueArr.join("\n"));
-        password.style.borderColor = alertRedInput;
-    } else {
-        password.setCustomValidity("");
-        password.style.borderColor = defaultInput;
-    }
-}
