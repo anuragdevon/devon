5b1fd2805e622b2a511155b00de89af2b4987f35
anuragdevon <msm19b021@iiitdm.ac.in>
2021-06-26 02:54:12 +0530

    optimized

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,10 +1,3 @@
-// <== Google Analytics ==>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-200019838-1');
-
 //<== contact form ==>
 function openForm() {
     document.getElementById("myForm").style.display = "block";
